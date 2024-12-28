d34965727734ccb1b70903c8afd4450ec51d838b
NguyenKhoi <ngnguyenkhoib4@gmail.com>
2024-12-28 01:43:53 +0000

    Update media Repository

--- src/Repositories/Files/MediaFileRepositoryInterface.php
+++ src/Repositories/Files/MediaFileRepositoryInterface.php
@@ -8,7 +8,6 @@ interface MediaFileRepositoryInterface extends MediaBaseRepositoryInterface
 {
     public function filter(array $data);
 
-
     public function findByFolderId($folderId);
 
     public function updateFileByPermalink($permalink, array $data);
